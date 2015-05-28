4a4dbd7f922a0fc11643b3edea1ccead8bba2fc6
Jungwon <jungwon81@gmail.com>
2015-05-27 22:03:52 -0700

    Add ecore event handler in app-relay-framework

--- packaging/app-relay-framework.spec
+++ packaging/app-relay-framework.spec
@@ -16,9 +16,11 @@ BuildRequires:  pkgconfig(glib-2.0)
 BuildRequires:  pkgconfig(capi-network-bluetooth)
 BuildRequires:  pkgconfig(capi-system-info)
 BuildRequires:  pkgconfig(dlog)
-BuildRequires: pkgconfig(glib-2.0)
-BuildRequires: pkgconfig(vconf)
-BuildRequires: pkgconfig(heynoti)
+BuildRequires:  pkgconfig(glib-2.0)
+BuildRequires:  pkgconfig(vconf)
+BuildRequires:  pkgconfig(heynoti)
+BuildRequires:  pkgconfig(ecore-x)
+BuildRequires:  pkgconfig(ecore-evas)
 
 ## Description string that this package's human users can understand
 %description
--- server/src/main.cpp
+++ server/src/main.cpp
@@ -9,6 +9,9 @@
 #include <dlog.h>
 #include <glib.h>
 #include <bluetooth.h>
+#include <Ecore.h>
+#include <Ecore_X.h>
+#include <Ecore_Evas.h>
 
 #undef LOG_TAG
 #define LOG_TAG "REMOTE_KEY_FW"
@@ -100,6 +103,60 @@ void _vconf_noti_callback(keynode_t *node, void* data)
 
 #endif		
 
+}
+
+Eina_Bool mp_app_mouse_event_cb(void *data, int type, void *event)
+{
+	printf("TEST\n");
+	if (type == ECORE_EVENT_MOUSE_BUTTON_DOWN) {
+		printf("ECORE_EVENT_MOUSE_BUTTON_DOWN\n");
+	}
+	else if (type == ECORE_EVENT_MOUSE_BUTTON_UP) {
+		printf("ECORE_EVENT_MOUSE_BUTTON_UP\n");
+	}
+
+	return 0;
+}
+
+bool initEcore()
+{
+	printf("initEcore()\n");
+
+	int ret, type;
+	Eina_Bool did = EINA_FALSE;
+	Ecore_Event_Handler *mouse_down = NULL;
+	Ecore_Event_Handler *handler = NULL;
+	Ecore_Event *event;
+
+	ret = ecore_init();
+	if (ret != 1)
+		printf("ecore_init fail\n");
+
+	ecore_event_init();
+	type = ecore_event_type_new();
+	if (type < 1) 
+		printf("type fail\n");
+
+	handler = ecore_event_handler_add(type, mp_app_mouse_event_cb, &did);
+	if (!handler) 
+		printf("Regi fail 1\n");
+
+	event = ecore_event_add(type, NULL, NULL, NULL);
+	if (!event)
+		printf("add fail\n");
+
+
+	mouse_down = ecore_event_handler_add(ECORE_EVENT_MOUSE_BUTTON_DOWN, mp_app_mouse_event_cb, NULL);
+	if (!mouse_down)
+		printf("Regi fail 2\n");
+
+	printf("%d %d\n", type, ECORE_EVENT_MOUSE_BUTTON_DOWN);
+
+	printf("main_loop_bengin()\n");
+	ecore_main_loop_begin();
+
+	ret = ecore_shutdown();
+	printf("unreached main_loop_bengin()\n");
 }
 
 bool initVconf()
@@ -369,7 +426,9 @@ int main(int argc, char *argv[])
 
 	// Initialize vconf environments
 	initVconf();
-
+	
+	// Init ecore
+	initEcore();
 #if 0	// <--- ignore temporarily
 	// Initialize bluetooth
 	error = rkf_initialize_bluetooth(device_name);
